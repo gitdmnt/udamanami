3a58cce56cb393b63bc588738274a80a2f87fd74
Gitdmnt <100859051+gitdmnt@users.noreply.github.com>
2025-04-16 13:41:52 +0900

    refactor: jail

--- src/commands/jail.rs
+++ src/commands/jail.rs
@@ -26,84 +26,91 @@ pub fn register() -> CreateCommand {
         )
         .add_option(
             CreateCommandOption::new(CommandOptionType::Integer, "term", "刑期（秒）")
-                .required(false),
+                .required(false)
+                .max_int_value(0)
+                .max_int_value(3600),
         )
 }
 
 pub async fn run(options: &[ResolvedOption<'_>], ctx: &CommandContext<'_>) -> String {
-    let user = match options[0].value {
-        ResolvedValue::User(user, _) => user.id,
-        _ => {
-            return "誰？".to_owned();
-        }
-    };
-    let jailterm = match options[1].value {
-        ResolvedValue::Integer(term) => {
-            if term > JAIL_TERM_MAX.as_secs() as i64 {
-                JAIL_TERM_MAX
-            } else {
-                Duration::from_secs(term as u64)
-            }
-        }
-        _ => JAIL_TERM_DEFAULT,
-    };
+    // 引数からユーザと刑期を取得
+    let (user, jailterm) = options.iter().fold(
+        (None, JAIL_TERM_DEFAULT),
+        |(user, jailterm), option| match (option.name, &option.value) {
+            ("user", ResolvedValue::User(user, _)) => (Some(*user), jailterm),
+            ("term", ResolvedValue::Integer(term)) => (user, Duration::from_secs(*term as u64)),
+            _ => (user, jailterm),
+        },
+    );
+    let user = user.unwrap().id;
     let bot = ctx.bot;
     let guild = bot.guild_id;
     let roles = vec![bot.jail_mark_role_id, bot.jail_main_role_id];
+
+    // 指定ユーザーにロールを追加
     let member = guild.member(&ctx.cache_http(), user).await.unwrap();
     if member.add_roles(&ctx.cache_http(), &roles).await.is_err() {
         return "収監に失敗したよ".to_owned();
     }
-    let jail_term_end = Instant::now() + jailterm;
-    let content = if let Some((_, end)) = bot.jail_process.get(&user).map(|r| *r.value()) {
+
+    // 既存の収監状況確認とメッセージ生成
+    let now = Instant::now();
+    let jail_term_end = now + jailterm;
+    let result = if let Some((_, end)) = bot.jail_process.get(&user).map(|r| *r.value()) {
         if end > jail_term_end {
-            let content = format!(
+            Err(format!(
                 "{}はすでに収監中だよ（残り刑期：{}秒）",
                 user.mention(),
-                end.duration_since(Instant::now()).as_secs()
-            );
-            Err(content)
+                end.duration_since(now).as_secs()
+            ))
         } else {
-            let content = format!(
+            Ok(format!(
                 "{}を再収監したよ（残り刑期：{}秒 → {}秒）",
                 user.mention(),
-                end.duration_since(Instant::now()).as_secs(),
+                end.duration_since(now).as_secs(),
                 jailterm.as_secs()
-            );
-            Ok(content)
+            ))
         }
     } else {
-        let content = format!(
+        Ok(format!(
             "{}を収監したよ（刑期：{}秒）",
             user.mention(),
             jailterm.as_secs()
-        );
-        Ok(content)
+        ))
+    };
+    if let Err(result) = result {
+        return result;
     };
 
-    let Ok(content) = content else {
-        return content.unwrap_err();
-    };
-
-    let Some(newid) = bot.jail_id.lock().map_or(None, |mut oldid| {
-        *oldid += 1;
-        Some(*oldid)
-    }) else {
+    // 新しい識別子を生成して収監情報を更新
+    let newid = if let Ok(mut guard) = bot.jail_id.lock() {
+        *guard += 1;
+        *guard
+    } else {
         return "再収監に失敗したよ".to_owned();
     };
-
-    let reply = *ctx.channel_id;
-    let ctx = ctx.ctx.clone();
-    let roles = roles.to_vec();
     bot.jail_process.insert(user, (newid, jail_term_end));
+
+    // 非同期で刑期後の解除処理をスケジュール
+    let reply = *ctx.channel_id;
+    let ctx_clone = ctx.ctx.clone();
     let process = bot.jail_process.clone();
     spawn(async move {
         sleep(jailterm).await;
-        unjail::unjail(&reply, &ctx, &user, &guild, &roles, Some(newid), &process).await;
-        drop(process);
+        unjail::unjail(
+            &reply,
+            &ctx_clone,
+            &user,
+            &guild,
+            &roles,
+            Some(newid),
+            &process,
+        )
+        .await;
     });
-    content
+    result.unwrap()
 }
+
 pub async fn run_old(ctx: &CommandContext<'_>) {
     let reply = ctx.channel_id;
     let args = &ctx.args()[..];
