a46dc05e059f1f70f40ee0a1ab15d115fc7f2b5d
Gitdmnt <100859051+gitdmnt@users.noreply.github.com>
2025-05-01 14:42:21 +0900

    feat: add slash command support for isprime command

--- src/commands/isprime.rs
+++ src/commands/isprime.rs
@@ -1,17 +1,22 @@
 use crate::commands::CommandContext;
 
 use crate::commands::ManamiPrefixCommand;
+
+use crate::commands::ManamiSlashCommand;
+
+use serenity::model::application::ResolvedValue;
+
 pub const PREFIX_ISPRIME_COMMAND: ManamiPrefixCommand = ManamiPrefixCommand {
     name: "isprime",
     alias: &[],
     usage: "!isprime <n>",
     description: "nが素数かどうかを判定するよ！",
-    run: |ctx| Box::pin(run(ctx)),
+    run: |ctx| Box::pin(run_old(ctx)),
     is_dm_command: true,
     is_guild_command: true,
 };
 
-pub async fn run(ctx: CommandContext<'_>) {
+async fn run_old(ctx: CommandContext<'_>) {
     let command_args = ctx.args();
 
     if command_args.len() != 1 {
@@ -30,7 +35,51 @@ pub async fn run(ctx: CommandContext<'_>) {
         return;
     };
 
-    let (is_prime, factor) = match num {
+    let (is_prime, factor) = check_is_prime(num);
+
+    let is_prime = message(num, is_prime, factor);
+    ctx.channel_id
+        .say(ctx.cache_http(), is_prime)
+        .await
+        .unwrap();
+}
+
+pub const SLASH_ISPRIME_COMMAND: ManamiSlashCommand = ManamiSlashCommand {
+    name: "isprime",
+    usage: "/isprime <n>",
+    description: "nが素数かどうかを判定するよ！",
+    register,
+    run: |options, _| Box::pin(async move { run(options) }),
+    is_local_command: false,
+};
+
+fn register() -> serenity::builder::CreateCommand {
+    serenity::builder::CreateCommand::new("isprime")
+        .description("nが素数かどうかを判定するよ！")
+        .add_option(
+            serenity::builder::CreateCommandOption::new(
+                serenity::model::application::CommandOptionType::Integer,
+                "n",
+                "判定したい数字",
+            )
+            .min_int_value(0)
+            .required(true),
+        )
+}
+
+fn run(options: Vec<serenity::model::application::ResolvedOption<'_>>) -> String {
+    let ResolvedValue::Integer(num) = options[0].value else {
+        return "わかんないよ".to_owned();
+    };
+    let num = num as u64;
+
+    let (is_prime, factor) = check_is_prime(num);
+
+    message(num, is_prime, factor)
+}
+
+fn check_is_prime(num: u64) -> (bool, Vec<u64>) {
+    match num {
         0 | 1 => (false, vec![]),
         2 => (true, vec![2]),
         _ => {
@@ -63,21 +112,19 @@ pub async fn run(ctx: CommandContext<'_>) {
                 (false, factor)
             }
         }
-    };
+    }
+}
 
-    let is_prime = format!(
+fn message(num: u64, is_prime: bool, factor: Vec<u64>) -> String {
+    format!(
         "{}は{}",
         num,
         if is_prime {
-            "素数だよ".to_owned()
+            "素数だよ！".to_owned()
         } else if factor.is_empty() {
             "素数じゃないよ。あたりまえでしょ？".to_owned()
         } else {
-            format!("素数じゃないよ。素因数は{factor:?}だよ")
+            format!("素数じゃないよ。素因数は{factor:?}だよ。")
         }
-    );
-    ctx.channel_id
-        .say(ctx.cache_http(), is_prime)
-        .await
-        .unwrap();
+    )
 }
