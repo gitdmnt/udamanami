3f8a0000b3b97c1697569db4d503946d5c916287
boletales <30789006+boletales@users.noreply.github.com>
2025-05-01 12:21:12 +0900

    コマンド：!fetch

--- src/commands/fetch.rs
+++ src/commands/fetch.rs
@@ -1,73 +1,133 @@
-use crate::ai::GeminiModel;
-use serenity::{
-    builder::{CreateCommand, CreateCommandOption},
-    model::application::{CommandOptionType, ResolvedValue},
-};
-use std::time::Duration;
+use serenity::all::{GetMessages, UserId};
+use std::{cmp::min, collections::HashMap};
 
-use crate::{commands::ManamiSlashCommand, db::BotDatabase, Bot};
-use serenity::model::application::ResolvedOption;
+use crate::commands::ManamiPrefixCommand;
 
-const COMMAND_NAME: &str = "fetch";
-
-pub const SLASH_FETCH_COMMAND: ManamiSlashCommand = ManamiSlashCommand {
-    name: COMMAND_NAME,
-    usage: "/fetch [count]",
-    description: "チャンネルのログを読み込むよ！",
-    register,
-    run: |option, bot| {
-        let opts = parse_options(option, bot);
-        Box::pin(async move { run_body(opts, bot).await })
-    },
-    is_local_command: true,
+use super::CommandContext;
+pub const PREFIX_FETCH_COMMAND: ManamiPrefixCommand = ManamiPrefixCommand {
+    name: "fetch",
+    alias: &[],
+    usage: "!fetch [count]",
+    description: "このチャンネルに投稿された、覚えているのより古いメッセージを取得するよ！",
+    run: |ctx| Box::pin(run(ctx)),
+    is_dm_command: false,
+    is_guild_command: true,
 };
 
-pub fn register() -> CreateCommand {
-    CreateCommand::new(COMMAND_NAME)
-        .description("[sec]秒以内の連続した会話に対して、[model]を使って必ず返信するよ")
-        .add_option(
-            CreateCommandOption::new(CommandOptionType::Integer, "count", "取得件数")
-                .required(false)
-                .min_int_value(0)
-                .max_int_value(1000),
-        )
-}
+const FETCH_DEFAULT: u64 = 100;
+const FETCH_MAX: u64 = 1000;
 
-pub async fn run(option: Vec<ResolvedOption<'_>>, bot: &Bot) -> String {
-    run_body(parse_options(option, bot), bot).await
-}
+pub async fn run(ctx: CommandContext<'_>) {
+    let count = ctx
+        .args()
+        .first()
+        .and_then(|arg| arg.parse::<u64>().ok())
+        .unwrap_or(FETCH_DEFAULT)
+        .clamp(0, FETCH_MAX);
 
-fn parse_options(option: Vec<ResolvedOption<'_>>, bot: &Bot) -> u32 {
-    let count = option
-        .iter()
-        .fold(None, |count, option| match (option.name, &option.value) {
-            ("count", ResolvedValue::Integer(i)) => Some(*i as u32),
-            _ => count,
-        })
-        .unwrap_or(100);
+    let mut oldest = ctx
+        .bot
+        .database
+        .fetch_oldest_message(&ctx.channel_id)
+        .await
+        .ok()
+        .flatten()
+        .map(|m| m.message_id);
 
-    count
-}
+    let oldest_start = oldest;
 
-async fn run_body(count: u32, bot: &Bot) -> String {
-    let mut response = String::new();
+    let mut remaining = count;
+    let mut log = Vec::new();
+    while remaining > 0 {
+        println!("remaining: {}, oldest: {:?}", remaining, oldest);
 
-    let oldest = bot.database.fetch_oldest_message().await;
+        let mut messages = ctx
+            .channel_id
+            .messages(
+                ctx.cache_http(),
+                oldest.map_or_else(
+                    || GetMessages::new().limit(min(remaining, 100) as u8),
+                    |oldest| {
+                        GetMessages::new()
+                            .limit(min(remaining, 100) as u8)
+                            .before(oldest)
+                    },
+                ),
+            )
+            .await
+            .unwrap_or_default();
 
-    match oldest {
-        Some(oldest) => {
-            let messages = bot
-                .channel_id
-                .messages(&bot.http, |m| m.before(oldest).limit(count))
-                .await
-                .unwrap();
+        let count = messages.len();
+        oldest = messages.last().map(|m| m.id);
 
-            for message in messages {
-                response.push_str(&format!("{}: {}\n", message.author.name, message.content));
-            }
+        log.append(&mut messages);
+
+        if count == 0 {
+            break;
         }
-        None => {
-            response.push_str("No messages found.");
+        remaining -= count as u64;
+    }
+
+    let default_channel_name = "".to_owned();
+    let channel_name = ctx
+        .channel_id
+        .name(&ctx.cache_http())
+        .await
+        .unwrap_or(default_channel_name);
+
+    let mut user_info: HashMap<UserId, String> = HashMap::new();
+    for message in &log {
+        if user_info.contains_key(&message.author.id) {
+            continue;
+        }
+
+        let user_name = message.author_nick(&ctx.cache_http()).await;
+        let user_name = user_name
+            .as_deref()
+            .unwrap_or_else(|| message.author.display_name());
+
+        user_info.insert(message.author.id, user_name.to_owned());
+    }
+
+    let _ = ctx
+        .bot
+        .database
+        .insert_many_guild_messages(&log, user_info, (ctx.channel_id, channel_name))
+        .await;
+
+    let oldest = ctx
+        .bot
+        .database
+        .fetch_oldest_message(&ctx.channel_id)
+        .await
+        .ok()
+        .flatten()
+        .map(|m| m.message_id);
+
+    if let Some(oldest) = oldest {
+        if let Some(oldest_start) = oldest_start {
+            ctx.channel_id
+                .say(
+                    ctx.cache_http(),
+                    format!(
+                        "メッセージ読み込み: {} → {}",
+                        oldest.link(ctx.channel_id, ctx.guild_id),
+                        oldest_start.link(ctx.channel_id, ctx.guild_id)
+                    ),
+                )
+                .await
+                .ok();
+        } else {
+            ctx.channel_id
+                .say(
+                    ctx.cache_http(),
+                    format!(
+                        "メッセージ読み込み: {}",
+                        oldest.link(ctx.channel_id, ctx.guild_id)
+                    ),
+                )
+                .await
+                .ok();
         }
     }
 }
